fc4eef78816205c42c3e603f7f5ff2f07aa07042
Zane <zeisen33@gmail.com>
2023-02-06 14:49:51 -0500

    Backend Auth part 1

---
+++ 200-OK/app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :display_name, presence: true, uniqueness: true 
+    validates :display_name, length: { in: 3..30 }, format: {without: URI::MailTo::EMAIL_REGEXP, message: "Cant be an email"}
+    validates :email, presence: true, uniqueness: true
+    validates :email, length: { in: 3..255 }, format: {with: URI::MailTo::EMAIL_REGEXP}
+    # validates :password_digest, presence: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: {in: 6..255, allow_nil: true}
+
+    before_validation :ensure_session_token
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        session_token
+      end
+    
+    def self.find_by_credentials (email, password)
+        user = User.find_by(email: email)
+    
+        if user&.authenticate(password)
+          user
+        else
+          nil
+        end
+    end
+    
+
+    private
+
+    def generate_unique_session_token
+        while true
+            token = SecureRandom.urlsafe_base64
+      
+            return token unless User.exists?(session_token: token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+end
