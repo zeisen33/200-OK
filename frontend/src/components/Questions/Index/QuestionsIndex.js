cafaba775cc4657a27b00b62810f9c435720ea20
Zane <zeisen33@gmail.com>
2023-04-05 18:04:22 -0400

    QuestionsIndex styling ok

--- frontend/src/components/Questions/Index/QuestionsIndex.js
+++ frontend/src/components/Questions/Index/QuestionsIndex.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import * as questionActions from '../../../store/questions.js'
 import * as userActions from '../../../store/users'
+import './QuestionsIndex.css'
 
 const QuestionsIndex = () => {
     const dispatch = useDispatch()
@@ -22,28 +23,40 @@ const QuestionsIndex = () => {
 
     const questionsList = questions.map((question) => {    
         return (
-            <div key={question.id} >
-                <ul>
-                    <li>
-                        <h1>
-                            <Link to={`/questions/${question.id}`}>{question.title}</Link>
-                        </h1>
-                    </li>
-                    <li>
-                        Asked by: {users[question.askerId].displayName}
-                    </li>
-                </ul>
-            </div>
+            <section>
+                <div key={question.id} >
+                    <ul id='sectionContentContainer'>
+                        <li id='TitleContainer'>
+                            <h1 className='QTitle'>
+                                <Link to={`/questions/${question.id}`}>{question.title}</Link>
+                            </h1>
+                        </li>
+                        <div id='askedByContainer'>
+                            <li className='askedBy'>
+                                Asked by: {users[question.askerId].displayName}
+                            </li>
+                        </div>
+                    </ul>
+                </div>
+            </section>
+            
         )
     })
 
     // debugger
 
     return (
-        <>
-            <h1>Questions</h1>
+        <div id='OuterQIndexContainer'>
+            <div id='TopSec'>
+                <div id='TopQsContainer'>
+                    <h1 id='TopQs'>Top Questions</h1>
+                </div>
+                <div id='NewQContainer'>
+                    <Link id='AskButton' to='/questions/new' >Ask Question</Link>
+                </div>
+            </div>
             {questionsList}
-        </>
+        </div>
     )
 }
 
