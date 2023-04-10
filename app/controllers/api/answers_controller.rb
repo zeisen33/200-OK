e6db6ab282463324f9c25ed3f8d9b28943666e7a
Zane <zeisen33@gmail.com>
2023-04-10 12:35:58 -0400

    Answer controller

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -5,14 +5,15 @@ class Api::AnswersController < ApplicationController
     end
 
     def show
-        @answer = Answer.find(params[:id])
+        @answer = Answer.find_by(id: params[:id])
         # @answer_author = User.find(@answer.answer_author_id)
         render :show
     end
 
     def create
         @answer = Answer.new(answer_params)
-        @answer.author = current_user
+        ## how do i get @answer[:question_id] and @answer[], which it needs
+        @answer[:author_id] = current_user.id
         if @answer.save
             render :show
         else
@@ -21,15 +22,27 @@ class Api::AnswersController < ApplicationController
     end
 
     def update
-
+        @answer = Answer.find_by(id: params[:id])
+        @current_user = current_user
+        if @answer.answer_author.id == @current_user.id 
+            if @answer.update(answer_params)
+                render json: @answer
+            else
+                render json: { errors: @answer.errors.full_messages }
+            end
+        end
     end
 
     def destroy
-
+        @answer = Answer.find_by(id: params[:id])
+        @current_user = current_user
+        if @answer.answer_author.id == @current_user.id
+            @answer.delete
+        end
     end
 
     private
     def answer_params
-        params.require(:answer).permit(:body)
+        params.require(:answer).permit(:body, :question_id)
     end
 end
