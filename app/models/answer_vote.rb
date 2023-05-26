69896f5bf9e03986cc90816ee73af0342cd848c1
Zane <zeisen33@gmail.com>
2023-05-26 14:29:27 -0400

    Vote validation

--- app/models/answer_vote.rb
+++ app/models/answer_vote.rb
@@ -1,2 +1,7 @@
 class AnswerVote < ApplicationRecord
+    validates :voter_id, presence: true, uniqueness: {scope: :voted_answer_id, message: 'You have already voted on this answer'}
+    validates :voted_answer_id, presence: true
+    validates :direction, presence: true
+
+    
 end
