ae9de7310536de29d2ad10aefeff63640a4b8ce9
Zane <zeisen33@gmail.com>
2023-05-30 16:13:23 -0400

    Backend Start

--- app/controllers/api/answer_vote_controller.rb
+++ app/controllers/api/answer_vote_controller.rb
@@ -1,6 +1,9 @@
 class Api::AnswerVoteController < ApplicationController
+    wrap_parameters include: AnswerVote.attribute_names + ['voterId, votedAnswerId, direction']
+    
     def show
-        @answer_vote = answer_vote.find_by()
+        @answer_vote = AnswerVote.find_by(id: params[:id])
+        render :show
     end
     
     def index
@@ -8,7 +11,12 @@ class Api::AnswerVoteController < ApplicationController
     end
 
     def create
-
+        @answer_vote = Answer.new(answer_params)
+        if @answer_vote.save
+            render :show
+        else
+            render json: { errors: @answer.errors.full_messages }
+        end
     end
 
     def update
@@ -17,6 +25,11 @@ class Api::AnswerVoteController < ApplicationController
 
     def destroy
 
+    end
+
+    private
+    def answer_vote_params
+        params.require(:answer_vote).permit(:voter_id, :voted_answer_id, :direction)
     end
 
 end
---
+++ app/views/api/answer_vote/show.json.jbuilder
@@ -0,0 +1,11 @@
+json.answer_vote do
+    json.extract! @answer_vote, :id, :voter_id, :voted_answer_id, :direction
+end
+
+json.voter do
+    json.extract! @answer.voter, :id, :display_name
+end
+
+json.answer do
+    json.extract! @answer.answer, :id 
+end
